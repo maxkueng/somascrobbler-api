5de4aa2adf88c17853b42f3b2b19f69a442e8c82
Max Kueng <me@maxkueng.com>
2015-03-19 04:01:25 +0100

    added socket.io version and uptime ticker

--- api/get-version.js
+++ api/get-version.js
@@ -1,5 +1,8 @@
 var pkg = require('../package.json');
 
 exports = module.exports = function (request, reply) {
-	reply({ version: pkg.version });
+	reply({
+		version: pkg.version,
+		socketIo: pkg.dependencies['socket.io'].replace(/[^0-9.]/g, '')
+	});
 };
--- static/sample-version.js
+++ static/sample-version.js
@@ -1,3 +1,4 @@
 {
-  "version": "1.1.2"
+  "version": "1.1.2",
+  "socketIo": "1.3.5"
 }
